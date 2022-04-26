3189c43af8316ca3792d3611d84e26563af1e218
Abel Biwott <abelsumbeiywo@gmail.com>
2022-04-26 11:43:20 +0300

    changed config vars

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,14 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function() {
- const db = config.get('db');
-  mongoose.connect(db, {useNewUrlParser:true, useUnifiedTopology: true})
- // mongoose.connect(db)
- //a small cheat
- //another..
+module.exports = function () {
+  const db = config.get("db");
+  mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    // mongoose.connect(db)
+    //a small cheat
+    //another..
+    //yet another...
 
     .then(() => winston.info(`Connected to ${db}...`));
-}
+};
